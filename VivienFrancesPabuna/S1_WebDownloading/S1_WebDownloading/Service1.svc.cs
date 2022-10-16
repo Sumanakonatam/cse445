5e9e8c665e29d358d96ea4c32ce44b3d52adccd3
kvfp <vfpabuna@gmail.com>
2022-10-16 11:52:30 -0700

    Cleanup

--- VivienFrancesPabuna/S1_WebDownloading/S1_WebDownloading/Service1.svc.cs
+++ VivienFrancesPabuna/S1_WebDownloading/S1_WebDownloading/Service1.svc.cs
@@ -29,28 +29,10 @@ namespace Assignment1
         */
         public string getContent(string url)
         {
-            //url = "https://venus.sod.asu.edu/WSRepository/Services/RandomString/Service.svc/GetRandomString/20";
-            string remoteUri = url;
-            Uri ServivrUri = new Uri(url);
-            //WebClient proxy = new WebClient();
-            //byte[] abc = proxy.DownloadData(ServivrUri);
-            //System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
-            //string str = encoding.GetString(abc);
-            //XElement xmlroot = XElement.Parse(str);
-            //string txtContent = ((XElement)(xmlroot)).Value;
-            //return txtContent;
-            // Create a new WebClient instance.
-            WebClient myWebClient = new WebClient();
-            // Download home page data.
-            // Download the Web resource and save it into a data buffer.
-            byte[] myDataBuffer = myWebClient.DownloadData(remoteUri);
-            System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
-            // Display the downloaded data.
-            string str = encoding.GetString(myDataBuffer);
-
             WebRequest request = WebRequest.Create(url);
             WebResponse response = request.GetResponse();
             Stream data = response.GetResponseStream();
+
             string html = String.Empty;
             using (StreamReader sr = new StreamReader(data))
             {
